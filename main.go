fc24ae063aaee3e301310ceb7658963e68c5e70a
Alexander Myasnikov <myasnikov.alexander.s@gmail.com>
2025-05-10 12:56:31 +0300

    refactoring

--- main.go
+++ main.go
@@ -15,11 +15,8 @@ import (
 const (
 	screenWidth  = 640
 	screenHeight = 640
-)
-
-var (
-	maxIters = 1000
-	cCoeff   = complex(-0.74543, 0.11301)
+	maxIters     = 1000
+	cCoeff       = complex(-0.74543, 0.11301)
 )
 
 type Point struct {
@@ -35,87 +32,104 @@ type Game struct {
 	width  int
 	height int
 
-	x0, y0, dx0, dy0 float64
+	CenterX, CenterY float64
+	ScaleX, ScaleY   float64
 
-	points []Point
+	points      []Point
+	needsUpdate bool
 }
 
 func NewGame(width, height int) *Game {
 	g := &Game{
-		width:  width,
-		height: height,
-		x0:     0,
-		y0:     0,
-		dx0:    1,
-		dy0:    1,
-		points: nil,
+		width:       width,
+		height:      height,
+		CenterX:     0,
+		CenterY:     0,
+		ScaleX:      1,
+		ScaleY:      1,
+		points:      nil,
+		needsUpdate: true,
 	}
 
 	return g
 }
 func (g *Game) Update() error {
-	if g.points == nil {
-		log.Println("update")
-		for x := range g.width {
-			for y := range g.height {
-				xx := norm(float64(x), float64(g.width)/2, float64(g.width)/2, g.x0, g.dx0)
-				yy := norm(float64(y), float64(g.width)/2, float64(g.width)/2, g.y0, g.dy0)
+	g.handleInput()
 
-				col := calc(xx, yy, maxIters, cCoeff)
-				g.points = append(g.points, Point{x, y, col})
-			}
-		}
-		log.Println("update ok")
-	}
-
-	scaleSize := 0.5
-	if inpututil.IsKeyJustPressed(ebiten.KeyA) {
-		g.points = nil
-		g.x0 -= scaleSize * g.dx0
-	}
-	if inpututil.IsKeyJustPressed(ebiten.KeyD) {
-		g.points = nil
-		g.x0 += scaleSize * g.dx0
-	}
-	if inpututil.IsKeyJustPressed(ebiten.KeyW) {
-		g.points = nil
-		g.y0 -= scaleSize * g.dy0
-	}
-	if inpututil.IsKeyJustPressed(ebiten.KeyS) {
-		g.points = nil
-		g.y0 += scaleSize * g.dy0
-	}
-	if inpututil.IsKeyJustPressed(ebiten.KeyQ) {
-		g.points = nil
-		g.dx0 += scaleSize * g.dx0
-		g.dy0 += scaleSize * g.dy0
-	}
-	if inpututil.IsKeyJustPressed(ebiten.KeyE) {
-		g.points = nil
-		g.dx0 -= scaleSize * g.dx0
-		g.dy0 -= scaleSize * g.dy0
-	}
-	if inpututil.IsKeyJustPressed(ebiten.KeyU) {
-		g.points = nil
-	}
-	if inpututil.IsKeyJustPressed(ebiten.KeyT) {
-		// tmp
-		// g.points = nil
-	}
-	if inpututil.IsKeyJustPressed(ebiten.KeyZ) {
-		os.Exit(1)
+	if g.needsUpdate {
+		log.Println("Generating fractal...")
+		g.generatePoints()
+		g.needsUpdate = false
+		log.Println("Fractal updated.")
 	}
 
 	return nil
 }
 
+func (g *Game) handleInput() {
+	const scaleStep = 0.3
+	if inpututil.IsKeyJustPressed(ebiten.KeyA) {
+		g.CenterX -= scaleStep * g.ScaleX
+		g.needsUpdate = true
+	}
+	if inpututil.IsKeyJustPressed(ebiten.KeyD) {
+		g.CenterX += scaleStep * g.ScaleX
+		g.needsUpdate = true
+	}
+	if inpututil.IsKeyJustPressed(ebiten.KeyW) {
+		g.CenterY -= scaleStep * g.ScaleY
+		g.needsUpdate = true
+	}
+	if inpututil.IsKeyJustPressed(ebiten.KeyS) {
+		g.CenterY += scaleStep * g.ScaleY
+		g.needsUpdate = true
+	}
+	if inpututil.IsKeyJustPressed(ebiten.KeyQ) {
+		g.ScaleX *= 1 + scaleStep
+		g.ScaleY *= 1 + scaleStep
+		g.needsUpdate = true
+	}
+	if inpututil.IsKeyJustPressed(ebiten.KeyE) {
+		g.ScaleX *= 1 - scaleStep
+		g.ScaleY *= 1 - scaleStep
+		g.needsUpdate = true
+	}
+	if inpututil.IsKeyJustPressed(ebiten.KeyU) {
+		g.needsUpdate = true
+	}
+	if inpututil.IsKeyJustPressed(ebiten.KeyZ) {
+		os.Exit(0)
+	}
+}
+
+func (g *Game) generatePoints() {
+	g.points = make([]Point, 0, g.width*g.height)
+
+	var (
+		centerX float64 = float64(g.width) / 2
+		centerY float64 = float64(g.height) / 2
+		scaleX  float64 = float64(g.width) / 2
+		scaleY  float64 = float64(g.height) / 2
+	)
+
+	for x := range g.width {
+		for y := range g.height {
+			nx := remapPoint(float64(x), centerX, scaleX, g.CenterX, g.ScaleX)
+			ny := remapPoint(float64(y), centerY, scaleY, g.CenterY, g.ScaleY)
+			col := computeColor(nx, ny, maxIters, cCoeff)
+			g.points = append(g.points, Point{x, y, col})
+		}
+	}
+}
+
 func (g *Game) Draw(screen *ebiten.Image) {
-	for _, point := range g.points {
-		screen.Set(point.X, point.Y, point.Color)
+	for _, p := range g.points {
+		screen.Set(p.X, p.Y, p.Color)
 	}
 
 	msg := fmt.Sprintf("TPS: %0.2f FPS: %0.2f\n", ebiten.ActualTPS(), ebiten.ActualFPS())
-	msg += fmt.Sprintf("p=(%v,%v) d=(%v,%v)\n", g.x0, g.y0, g.dx0, g.dy0)
+	msg += fmt.Sprintf("Center: (%v,%v), Scale: (%v,%v)\n", g.CenterX, g.CenterY, g.ScaleX, g.ScaleY)
+	msg += fmt.Sprintf("maxIters: %v, cCoeff: %v\n", maxIters, cCoeff)
 	ebitenutil.DebugPrint(screen, msg)
 }
 
@@ -123,26 +137,11 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return outsideWidth, outsideHeight
 }
 
-func main() {
-	log.SetFlags(log.Lshortfile | log.Lmicroseconds)
-
-	ebiten.SetWindowTitle("2d Demo")
-	ebiten.SetWindowSize(screenWidth, screenHeight)
-
-	game := NewGame(screenWidth, screenHeight)
-	if err := ebiten.RunGame(game); err != nil {
-		log.Fatal(err)
-	}
+func remapPoint(x, fromCenter, fromScale, toCenter, toScale float64) float64 {
+	return (x-fromCenter)*(toScale/fromScale) + toCenter
 }
 
-func norm(x, x0, dx0, x1, dx1 float64) float64 {
-	x -= x0
-	x *= dx1 / dx0
-	x += x1
-	return x
-}
-
-func calc(x, y float64, maxIters int, c complex128) color.RGBA {
+func computeColor(x, y float64, maxIters int, c complex128) color.RGBA {
 	z := complex(x, y)
 
 	for i := range maxIters {
@@ -150,10 +149,23 @@ func calc(x, y float64, maxIters int, c complex128) color.RGBA {
 			r := uint8((1*i + 17) % 255)
 			g := uint8((2*i + 01) % 255)
 			b := uint8((0*i + 01) % 255)
-			return color.RGBA{r, g, b, 0}
+			return color.RGBA{r, g, b, 255}
 		}
+
 		z = z*z + c
 	}
 
 	return color.RGBA{}
+}
+
+func main() {
+	log.SetFlags(log.Lshortfile | log.Lmicroseconds)
+
+	ebiten.SetWindowTitle("2D Fractal Viever - Demo")
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+
+	game := NewGame(screenWidth, screenHeight)
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatal(err)
+	}
 }
